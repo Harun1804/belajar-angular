123928d01ff4c62dcc2c42def939947311d38ca2
Harun1804 <Harun.arrasyid1804@gmail.com>
2022-12-20 09:01:52 +0700

    string interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,4 +9,12 @@ import {Component} from "@angular/core";
     }
   `]
 })
-export class ServerComponent {}
+
+export class ServerComponent {
+  serverId:number = 10;
+  serverStatus:string = 'offline';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+}
